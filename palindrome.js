6b9f49d0ab6d49b1a05a6dbfed6e063f6e3b20b5
Sumitamahato <mahtosumita5@gmail.com>
2022-11-02 18:55:28 +0530

    created the files of both palindrome and prime

---
+++ palindrome.js
@@ -0,0 +1,16 @@
+let str="naman"
+
+function reverse(str){
+
+ for(let i=0;i<=atr.length-1;i++){
+
+  bag+=str[i];
+}
+   return bag;
+
+} let reversed=reverse(str);
+   if(str=reversed){
+console.log("Palindrome");
+} else{
+console.log("NA")
+{
---
+++ prime.js
@@ -0,0 +1,15 @@
+function checkPrime(n){
+
+  for(let i=0;i<=num;i++){
+
+       if(num%i!=0){
+
+   return true;
+
+}
+   else{
+
+    return false
+
+}
+checkPrime(21);
